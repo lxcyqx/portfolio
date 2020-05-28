8324642b6dc755dfd3f05422a432d1087fee27ee
Lucy Qu <lucyqu@Lucys-MacBook-Pro.local>
2020-05-27 20:11:08 -0400

    change arrows in photography slide show

--- gallery.js
+++ gallery.js
@@ -36,18 +36,16 @@ if (galleryImages) {
 
       //execute code after image is loaded
       newImage.onload = function() {
-        let newNextBtn = document.createElement("a");
-        let btnNextText = document.createTextNode(">");
-        newNextBtn.appendChild(btnNextText);
+        let newNextBtn = document.createElement("img");
         container.appendChild(newNextBtn);
+        newNextBtn.setAttribute("src", "images/right-arrow.png");
         newNextBtn.setAttribute("class", "img-btn-next");
         newNextBtn.setAttribute("onclick", "changeImg(1)");
         newNextBtn.style.cssText = "right: 20px";
 
-        let newPrevBtn = document.createElement("a");
-        let btnPrevText = document.createTextNode("<");
-        newPrevBtn.appendChild(btnPrevText);
+        let newPrevBtn = document.createElement("img");
         container.appendChild(newPrevBtn);
+        newPrevBtn.setAttribute("src", "images/left-arrow.png");
         newPrevBtn.setAttribute("class", "img-btn-prev");
         newPrevBtn.setAttribute("onclick", "changeImg(0)");
         newPrevBtn.style.cssText = "left: 20px";
